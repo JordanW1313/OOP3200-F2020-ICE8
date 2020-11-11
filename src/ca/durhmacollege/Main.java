26864ab9cc44a7bfd89c973e1fe89925196eaa27
JordanW1313 <jwriker@gmail.com>
2020-11-11 10:32:16 -0500

    Included basic input and output test

--- src/ca/durhmacollege/Main.java
+++ src/ca/durhmacollege/Main.java
@@ -1,8 +1,22 @@
+/*
+* Author: Jordan Wriker
+* Date  : November 11, 2020
+*/
+
+
 package ca.durhmacollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %s",name);
     }
 }
